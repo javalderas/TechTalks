cc77131430055827864fb17413d4a855b437f32d
Juan Antonio Valderas <javalderas@heytrade.com>
2021-02-03 11:15:34 +0100

    RED stage of test

---
+++ TDDCalculator/src/main/kotlin/com/heytrade/techtalk/exercises/Calculator.kt
@@ -0,0 +1,7 @@
+package com.heytrade.techtalk.exercises
+
+class Calculator {
+    fun parse(s: String): Int {
+        return 0 // is a constant
+    }
+}
--- TDDCalculator/src/test/kotlin/com/heytrade/techtalk/exercises/CalculatorTest.kt
+++ TDDCalculator/src/test/kotlin/com/heytrade/techtalk/exercises/CalculatorTest.kt
@@ -8,6 +8,8 @@ class CalculatorTest {
 
     @Test
     fun testMultiply2by2() {
-        // TODO
+        val calculator = Calculator()
+        val result = calculator.parse("2 * 2")
+        assertEquals(4, result)
     }
 }
