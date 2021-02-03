96e45bbd2216516162d50d5f4780c7bc1753095f
Juan Antonio Valderas <javalderas@heytrade.com>
2021-02-03 12:32:53 +0100

    GREEN with DIVIDE operation

--- TDDCalculator/src/main/kotlin/com/heytrade/techtalk/exercises/Calculator.kt
+++ TDDCalculator/src/main/kotlin/com/heytrade/techtalk/exercises/Calculator.kt
@@ -2,10 +2,13 @@ package com.heytrade.techtalk.exercises
 
 class Calculator {
     fun parse(s: String): Int {
-        val(a, op, b)= s.split(" ")
+        val(a, op, b) = s.split(" ") // <---- EYE LOOK Declaration val!!
         return when (op) {
             "*" ->a.toInt() * b.toInt()
+            "/" ->a.toInt() / b.toInt()
             else -> throw IllegalArgumentException("invalid operator")
         }
     }
+
+    // No Corner Cases lost when you use TDD.
 }
--- TDDCalculator/src/test/kotlin/com/heytrade/techtalk/exercises/CalculatorTest.kt
+++ TDDCalculator/src/test/kotlin/com/heytrade/techtalk/exercises/CalculatorTest.kt
@@ -12,4 +12,20 @@ class CalculatorTest {
         val result = calculator.parse("2 * 2")
         assertEquals(4, result)
     }
+
+    @Test
+    fun testDivide2By2() {
+        val calculator = Calculator()
+        val result = calculator.parse("2 / 2")
+        assertEquals(1, result)
+    }
+
+    /**
+     *  Why use <internal> word? when execute no test on windows selected. Why?
+    @Test
+    internal fun testMultiply2by2() {
+    val calculator = Calculator()
+    val result = calculator.parse("2 * 2")
+    assertEquals(4, result)
+    }*/
 }
