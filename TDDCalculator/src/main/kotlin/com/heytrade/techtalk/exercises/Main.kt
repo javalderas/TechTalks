698a9dcd1292345bddbb81471e0033585c8efe2b
Juan Antonio Valderas <javalderas@heytrade.com>
2021-02-03 11:11:10 +0100

    First Commit: Preparation code

---
+++ TDDCalculator/src/main/kotlin/com/heytrade/techtalk/exercises/Main.kt
@@ -0,0 +1,11 @@
+package com.heytrade.techtalk.exercises
+
+class Main {
+
+    /**
+     * Main Method Application
+     */
+    fun main(args: Array<String>) {
+
+    }
+}
---
+++ TDDCalculator/src/test/kotlin/com/heytrade/techtalk/exercises/CalculatorTest.kt
@@ -0,0 +1,13 @@
+package com.heytrade.techtalk.exercises
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+// RED - GREEN - BLUE (refactor)
+class CalculatorTest {
+
+    @Test
+    fun testMultiply2by2() {
+        // TODO
+    }
+}
